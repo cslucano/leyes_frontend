bf0181cecda3fd46db3d6213bd5ebce9c112c2ea
Carlos Salvatierra <cslucano@gmail.com>
2015-02-21 04:17:07 -0500

    wip

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name leyesApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the leyesApp
+ */
+angular.module('leyesApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name leyesApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the leyesApp
+ */
+angular.module('leyesApp')
+  .controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
+
+  }]);
